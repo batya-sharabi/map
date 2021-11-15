7400ec10213c0a8106118edcd361a9f9ac0b9ac6
batya.sharabi <batya.sharabi@ngsoft.com>
2021-11-15 23:31:00 +0200

    map project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactBingmaps } from 'react-bingmaps';
+import Input from '@material-ui/core/Input';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className = "map-one">
+          
+          <h3>
+          Coordinates form
+            </h3>
+        <span>
+          Add by cords
+        </span>
+        <Input/>
+        <Input/>
+            <ReactBingmaps 
+              id = "one"
+              bingmapKey = "AjanoEGdVcD_Kj5XrRqHydJa2dOVszqIyvIJ9_yZBE-KWg17TNqyRixe9n2sltX4"
+              center = {[13.0827, 80.2707]}
+              zoom = {4}
+              className = "customClass"
+            > 
+            </ReactBingmaps>
+          </div>
     </div>
   );
 }
